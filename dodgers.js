d436a6393a5118670d5e0524a04ef7fe8213d1d4
Glitch (obvious-round-hummingbird) <none>
2020-12-17 16:27:35 +0000

    example for danielle

--- dodgers.js
+++ dodgers.js
@@ -15,13 +15,9 @@ let currentYear = null;
 
 
 $("button").click(function() {
-  console.log('year:', birthYear)
-  console.log('age:', age)
   birthYear = $(".year").val();
-  console.log('year:', birthYear)
   currentYear = $(".current-year").val();
   age = (currentYear - birthYear)
-  console.log('age:', age)
   name = $(".name").val();
   $(".message").html(`${name} is ${age} cool`);
 });
